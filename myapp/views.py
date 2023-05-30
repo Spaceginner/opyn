2b2556703aa6f96a9df764ab82d54238e6cc05b9
Spaceginner <ivan.demian2009@gmail.com>
2023-05-30 22:58:28 +0500

    removed leftover after me experimenting

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import loader
-
-
-# Create your views here.
 
 
 def index(request):
-    return render(request, "myapp/index.html", {})
+    return render(request, "myapp/index.html")
