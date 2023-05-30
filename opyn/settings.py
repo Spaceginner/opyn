7cf2a1c71144b314103089c1e8c23afe6619e0cb
Spaceginner <ivan.demian2009@gmail.com>
2023-05-30 22:42:20 +0500

    you didnt see that

--- opyn/settings.py
+++ opyn/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'myapp',
+    'myapp.apps.MyappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
