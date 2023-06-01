17a117b4f5fbfd32e6f12b32b04a13d5b45bc663
Spaceginner <ivan.demian2009@gmail.com>
2023-06-01 17:05:34 +0500

    add horizontal rule

--- myapp/modules/markdown.py
+++ myapp/modules/markdown.py
@@ -11,10 +11,11 @@ def _get_text_size(header_size: int) -> int:
 def compile_md(raw_markdown: str) -> str:
     html = ""
 
-    paragraph = ""
+    paragraph, previous_token_text = "", False
     md_lines = [line.strip() for line in raw_markdown.split('\n')]
     for i, md_line in enumerate(md_lines):
         if md_line.startswith('#'):  # header
+            previous_token_text = False
             # i believe there should be a dedicated builtin method for this
             count = 0
             while md_line[count] == '#':
@@ -33,13 +34,17 @@ def compile_md(raw_markdown: str) -> str:
                 text_size = _get_text_size(count)
                 html += f"<h{count} style=\"font-size: {text_size}rem\">" \
                         f"{md_line[count+1:].strip()}</h{count}>\n"
+        elif md_line in ['---', '***']:
+            previous_token_text = False
+            html += "<hr>\n"
         else:  # normal text
-            if md_lines[i] == '' and paragraph.strip():
+            if (md_lines[i] == '' or not previous_token_text) and paragraph.strip():
                 html += f"<p>{paragraph.strip()}</p><br>\n"
                 paragraph = ""
             elif i+1 == len(md_lines) and md_line.strip():
                 html += f"<p>{md_line.strip()}</p><br>\n"
             else:
                 paragraph += f" {md_line}"
+            previous_token_text = True
 
     return html
