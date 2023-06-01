34af022f9b88e3f446aa9818678fde108f52af03
Spaceginner <ivan.demian2009@gmail.com>
2023-06-01 21:25:21 +0500

    bold, underlined, striked and italics text and links support

--- myapp/modules/markdown.py
+++ myapp/modules/markdown.py
@@ -8,6 +8,60 @@ def _get_text_size(header_size: int) -> int:
     return HEADER_TEXT_SIZES[header_size-1]
 
 
+def parse_text(s: str) -> str:
+    modes: dict[str, bool] = {
+        'italics': False,
+        'bold': False,
+        'underlined': False,
+        'striked': False
+    }
+    buffer: str = ""
+    skip: int = 0
+    for i, char in enumerate(s):
+        if skip > 0: skip -= 1; continue  # NOQA E702
+        if s[i:i+2] == '**':
+            if modes['bold']:
+                modes['bold'] = False
+                buffer += "</strong>"
+            else:
+                modes['bold'] = True
+                buffer += "<strong>"
+            skip = 1
+        elif char == '*':
+            if modes['italics']:
+                modes['italics'] = False
+                buffer += "</em>"
+            else:
+                modes['italics'] = True
+                buffer += "<em>"
+        elif s[i:i+2] == '__':
+            if modes['underlined']:
+                modes['underlined'] = False
+                buffer += "</u>"
+            else:
+                modes['underlined'] = True
+                buffer += "<u>"
+            skip = 1
+        elif char == '[':
+            url_desc, url = "", ""
+            j, k = 0, 0
+            while s[i+1:][j] != ']':
+                if j+1 == len(s[i:]) : break  # NOQA E701
+                url_desc += s[i+1:][j]
+                j += 1
+            if s[i+j+2] == '(':
+                while s[i+j+2:][k] != ')':
+                    if k + 1 == len(s[i+j+2:]): break  # NOQA E701
+                    url += s[i+j+3:][k]
+                    k += 1
+            buffer += f"<a href=\"{url}\" target=\"_blank\">{url_desc}</a>"
+            skip = j+k+2
+
+        else:
+            buffer += char
+    return buffer
+
+
 def compile_md(raw_markdown: str) -> str:
     html = ""
 
@@ -22,13 +76,14 @@ def compile_md(raw_markdown: str) -> str:
                 count += 1
 
             if count > 6:  # ignore
-                if md_lines[i] == '' and paragraph.strip():
-                    html += f"<p>{paragraph.strip()}</p><br>\n"
+                if i + 1 == len(md_lines) and md_line:
+                    paragraph += f" {md_line}"
+                if (md_lines[i] == '' or not previous_token_text or i + 1 == len(md_lines)) and paragraph.strip():
+                    html += f"<p>{parse_text(paragraph.strip())}</p><br>\n"
                     paragraph = ""
-                elif i + 1 == len(md_lines) and md_line.strip():
-                    html += f"<p>{md_line.strip()}</p><br>\n"
                 else:
                     paragraph += f" {md_line}"
+                previous_token_text = True
 
             if md_line[count] == ' ':
                 text_size = _get_text_size(count)
@@ -38,11 +93,11 @@ def compile_md(raw_markdown: str) -> str:
             previous_token_text = False
             html += "<hr>\n"
         else:  # normal text
-            if (md_lines[i] == '' or not previous_token_text) and paragraph.strip():
-                html += f"<p>{paragraph.strip()}</p><br>\n"
+            if i+1 == len(md_lines) and md_line:
+                paragraph += f" {md_line}"
+            if (md_lines[i] == '' or not previous_token_text or i+1 == len(md_lines)) and paragraph.strip():
+                html += f"<p>{parse_text(paragraph.strip())}</p><br>\n"
                 paragraph = ""
-            elif i+1 == len(md_lines) and md_line.strip():
-                html += f"<p>{md_line.strip()}</p><br>\n"
             else:
                 paragraph += f" {md_line}"
             previous_token_text = True
